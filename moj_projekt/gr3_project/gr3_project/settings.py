e38f7753a858516aa12da2cc6ad2aac8164dff29
AdrianAlbrecht <adiran.albrecht99@gmail.com>
2025-10-17 09:58:32 +0200

    Lab_03

--- moj_projekt/gr3_project/gr3_project/settings.py
+++ moj_projekt/gr3_project/gr3_project/settings.py
@@ -31,10 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'biblioteka.apps.BibliotekaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
